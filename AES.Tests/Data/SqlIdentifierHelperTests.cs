91ffdf14679b556bee593d5829e76d2e799b1cc1
smcginn <smcginn@inteport.com>
2025-10-07 01:35:38 +0000

    End to End Sql implementation

--- AES.Tests/Data/SqlIdentifierHelperTests.cs
+++ AES.Tests/Data/SqlIdentifierHelperTests.cs
@@ -6,25 +6,6 @@ namespace AES.Tests.Data;
 
 public class SqlIdentifierHelperTests
 {
-    [Theory]
-    [InlineData("rubric", "[rubric]")]
-    [InlineData("dbo.rubric", "[dbo].[rubric]")]
-    [InlineData("[schema].table", "[[schema]].[table]")]
-    [InlineData("schema.sub.table", "[schema].[sub].[table]")]
-    public void FormatTableName_EscapesIdentifiers(string input, string expected)
-    {
-        var actual = SqlIdentifierHelper.FormatTableName(input);
-        Assert.Equal(expected, actual);
-    }
-
-    [Theory]
-    [InlineData(null!)]
-    [InlineData("")]
-    [InlineData(" ")]
-    public void FormatTableName_InvalidInput_Throws(string input)
-    {
-        Assert.Throws<ArgumentException>(() => SqlIdentifierHelper.FormatTableName(input));
-    }
 
     [Fact]
     public void ConvertToNullableInt_ParsesString()
