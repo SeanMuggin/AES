fb2714c8dd0d51af473000105e8246249ed653a6
Joe McGinn <jmcginn@inteport.com>
2025-10-10 12:16:35 -0700

    changed database from LH_DSP_AES to DW_DSP_AES, Changed Id to EssayId in Essay model, added SqlQueries folder and moved queries there

---
+++ AES/SqlQueries/essays_ModelTesting.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+      ROW_NUMBER() OVER (ORDER BY ar.Year ASC) AS EssayID
+      ,[Year]
+      ,[EssayType]
+      ,[EssayContent]
+      ,ar.ReaderId
+      ,StudentId
+      ,Score
+  FROM LH_DSP_AES.[dbo].[tblDSP_ApplicantReaderDetails] ar
+  join LH_DSP_AES.[dbo].[App_tblReader] r
+    on ar.ReaderId = r.ReaderId
+    and r.IsSuperReader = 1
+  where 
+    EssayType in ('DegreeFit','CollegeChoice')
+    and [Year] in (2023,2024,2025)
+    and ReaderScope = 'FinalSelection'
---
+++ AES/SqlQueries/rubric_ModelTesting.sql
@@ -0,0 +1,39 @@
+﻿WITH rubric_scores AS (
+    SELECT
+        appyear,
+        essay,
+        CONCAT(
+            'Score ',
+            score,
+            ': ',
+            STRING_AGG(
+                COALESCE(STRING_ESCAPE(score_criteria, 'json'), N''),
+                '; '
+            ) WITHIN GROUP (ORDER BY score_criteria)
+        ) AS ScoreLabel
+    FROM LH_DSP_AES.dbo.dsp_rubric
+    GROUP BY appyear, essay, score
+),
+rubric_summary AS (
+    SELECT
+        appyear AS [Year],
+        essay AS EssayType,
+        STRING_AGG(ScoreLabel, '. ') AS Rubric
+    FROM rubric_scores
+    GROUP BY appyear, essay
+),
+essay_questions AS (
+    SELECT
+        [Year],
+        EssayType,
+        CONCAT('Essay Question: ', Prompt) AS EssayQuestion
+    FROM [LH_DSP_AES].[dbo].[essay_questions]
+)
+SELECT
+    rs.[Year],
+    rs.EssayType,
+    CONCAT(eq.EssayQuestion, ' ', rs.Rubric) AS Rubric
+FROM rubric_summary rs
+JOIN essay_questions eq
+    ON rs.[Year] = eq.[Year]
+   AND rs.EssayType = eq.EssayType;
